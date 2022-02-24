c1b529629306a3a17b2f52e1181d20ca492396f4
Noah Horlacher <kuromarumusic@gmail.com>
2022-02-24 13:41:25 +0100

    highlight fix

--- js/main.js
+++ js/main.js
@@ -293,7 +293,7 @@ function submit_row() {
         // highlight keyboard chars
         UI.cells[selection.y].forEach(cell =>
             document.querySelectorAll('keyboard char[value="-1"], keyboard char[value="0"], keyboard char[value="1"]').forEach(el_char => {
-                if (el_char.textContent === cell.textContent && ['-1', '0', '1'].includes(el_char.getAttribute('value'))) el_char.setAttribute('value', cell.getAttribute('value'))
+                if (el_char.textContent === cell.value && ['-1', '0', '1'].includes(el_char.getAttribute('value'))) el_char.setAttribute('value', cell.getAttribute('value'))
             })
         )
     }
